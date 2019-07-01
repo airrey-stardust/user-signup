2c883f4f911a88b33c3d81c22bd67100c6d02c11
airrey-stardust <51347085+airrey-stardust@users.noreply.github.com>
2019-07-01 18:55:47 -0500

    Add files via upload
    added if statements for variables

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-# create a user sign up with username, password, verify password, and email
-# error when leaving fields empty
+
+
+
+
+######  To Do   #####
+# 1. error when fields left blank
+# 2. error when invalid characters
+# 3. character limits
+
+
+# DO NOT FORGET TO ACTIVATE FLASK IN GIT WHEN RUNNING - source activate flask-env ######
 
 from flask import Flask, render_template, request
 import jinja2
-import re
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
 @app.route('/', methods=['POST', 'GET'])
+
 #@app.route('/index')
-
-
 
 
 def index():
 
     #variable names and error messages
-    #inputs can not be empty     
+        
     username = ""
     email = ""
     password =""
@@ -30,36 +38,48 @@ def index():
 
     if request.method == 'POST':
 
-        #request information
+#### USERNAME #####
+    # add character limit?
         username = request.form['username']
 
-        if username == " ":
+        if username == "":
             error_username = "The username can not be blank."
 
         for i in username:
 
             if i.isspace():
-                # or username.isspace? add character limit?
+               
                 error_username = "Spaces are not allowed in the username."
+        
+        if (len(username) < 3) or (len(username) > 20):
+            error_username = "Usernames must be between 3 and 20 characters long."
 
 
-#########
+######### PAsSWORD #####
             
             password = request.form['password']
             
+            if password == "":
+                error_password = "The password can not be blank."
 
             for i in password:
                 
                 if i.isspace():
                     error_password = "Passwords can not have spaces."
+            
+            if (len(password) < 3) or (len(password) > 20):
+                error_password = "Passowrds must be between 3 and 20 characters long."
 
                 
 
-#####       
+#####   PASSWORD VERIFICATION ####
+    
             verify_password = request.form['verify_password']
            
-            
-            #redundant because of password = verification password in prev step
+            if verify_password = "":
+                error_password_verification = "The password verification can not be blank."
+            #redundant because of password = verification password in prev step?
+
             for i in verify_password:
 
                 if i.isspace():
@@ -67,37 +87,37 @@ def index():
 
             if password != verify_password:
                 error_password_verification = "The passwords do not match."
+
+####      EMAIL    ######
         
             email = request.form['email']
         #must contain @ and ., only one @?
-        #make allowed character set?
-        #what do most okay email logins have?
+        #make allowed character set? no {}[]? make a list?
+        #what do most valid email logins have?
         #email address may be blank
 
             #if email.count('@') != 1:
                 #error_email_message = "The email address must contain one @."
-            
+         
+            if (len(email) < 3) or (len(email) > 20):
+                error_email_message = "Email addresses must be between 3 and 20 characters long and contain an @."
+
             for i in email:
 
                 if i.isspace():
                     error_email_message = "The email address can not contain a space."
                 
+                
+                
 
-
-    #verify and validate information: no spaces, no invalid characters
-    #passwords are the same
-
-    #put in correct form to render
-    if (not error_username) and (not error_password) and (not error_password_verification) and (not error_email_message):   
-        return render_template('index.html', 
-            username=username, password=password, email=email, 
-            verify_password=verify_password, error_email_message=error_email_message, error_password=error_password, error_username=error_username,error_password_verification=error_password_verification)
+    if not error_username and not error_password and not error_password_verification and not error_email_message:   
+        
+        return render_template('index.html', username=username, password=password, email=email, verify_password=verify_password, error_email_message=error_email_message, error_password=error_password, error_username=error_username, error_password_verification=error_password_verification)
     
     
     return render_template('hello_user.html', username=username)
     
 
 
-### main
 
 app.run()

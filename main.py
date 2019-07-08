0df9295c7a7778bc3b749677d005da917460e910
airrey-stardust <51347085+airrey-stardust@users.noreply.github.com>
2019-07-08 15:22:43 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -18,17 +18,16 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/", methods=['POST', 'GET'])
+
 ### OR? #####
 #@app.route('/')
 
-def home():
-    return render_template('index.html')
 
-@app.route("/index", methods=['POST', 'GET'])
+
+
+
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    
-
     
 ######## USERNAME #####
     username = request.form['username']
@@ -36,38 +35,35 @@ def index():
     verify_password = request.form['verify_password']
     email = request.form['email']
 
-    username = ''
-    email = ''
-    password = ''
-    verify_password = ''
-    error_username = ''
-    error_password = ''
-    error_password_verification = ''
-    error_email_message = ''
+    username = ""
+    email = ""
+    password = ""
+    verify_password = ""
+    error_username = ""
+    error_password = ""
+    error_password_verification = ""
+    error_email_message = ""
     
-
-    if username == '':
-        error_username = "The username can not be blank."
-    if (len(username) < 3) or (len(username) > 20):
-        error_username = "Usernames must be between 3 and 20 characters long."
     for i in username:
         if i.isspace():
             error_username = "Spaces are not allowed in the username."
+    if username == "":
+            error_username = "The username can not be blank."
+    elif (len(username) < 3) or (len(username) > 20):
+        error_username = "Usernames must be between 3 and 20 characters long."
+        
+    
         
 
 ######### PAsSWORD #####
-            
-   
+    for i in password:       
+        if i.isspace():
+            error_username = "Spaces are not allowed in the username."
             
     if password == '':
         error_password = "The password can not be blank."
 
-    for i in password:
-                
-        if i.isspace():
-            error_password = "Passwords can not have spaces."
-            
-    if (len(password) < 3) or (len(password) > 20):
+    elif (len(password) < 3) or (len(password) > 20):
          error_password = "Passowrds must be between 3 and 20 characters long."
 
                 
@@ -110,9 +106,9 @@ def index():
                 
 ############# END TEXT VALIDATION #######           
                 
-    #### this needs to be changed, use Jinja to render the template####
-    if (error_username == '' and error_email_message == '' and error_password == '' and error_password_verification == ''):
-        return redirect('home')
+    
+    if (error_username == "" and error_email_message == "" and error_password == "" and error_password_verification == ""):
+        return redirect('home', username=username)
 
     else:
     #### this is good #####
@@ -122,7 +118,7 @@ def index():
 
 @app.route("/hello_user")
 def hello_user():
-    user = request.args.get('username')
-    return render_template('hello_user.html')
+    username = request.args.get('username')
+    return render_template('hello_user.html', username=username)
 
 app.run()
